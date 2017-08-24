ade4a06ef944c5c2bf922eb5e6e4ddd12362cf3c
alhulljar <31191366+alhulljar@users.noreply.github.com>
2017-08-23 23:24:12 -0400

    Completed Project
    Ready to Build

---
+++ (Prac) Roll-a-ball/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+
+    public float speed;
+    public Text scoreText;
+    public Text winText;
+
+    private Rigidbody rb;
+    private int count;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        count = 0;
+        winText.text = "";
+        SetScore();
+    }
+
+    void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(moveHorizontal, 0f, moveVertical);
+
+        rb.AddForce(movement * speed);
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Pickup"))
+        {
+            other.gameObject.SetActive(false);
+            count += 1;
+            SetScore();
+        }
+        
+    }
+
+    void SetScore()
+    {
+        scoreText.text = "Score: " + count.ToString();
+        if (count >= 16)
+        {
+            winText.text = "You Win!";
+        }
+    }
+
+}
